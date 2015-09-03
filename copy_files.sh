37ba133e11fa9580abe7ac0ac42aa2f122ce636a
Glen Horton <hortongn@ucmail.uc.edu>
2015-09-03 16:35:31 -0400

    Add .gitignore_global

--- copy_files.sh
+++ copy_files.sh
@@ -4,6 +4,7 @@ cp ~/.bashrc .bashrc
 cp ~/.bash_profile .bash_profile
 cp ~/.profile .profile
 cp ~/.gitconfig .gitconfig
+cp ~/.gitignore_global .gitignore_global
 cp ~/.vimrc .vimrc
 cp ~/.git-completion.bash .git-completion.bash
 rm -rf .vim

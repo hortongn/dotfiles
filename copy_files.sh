a63e4155f8efb93ae29c3edcb3c5e8e9a5031b36
Glen Horton <hortongn@ucmail.uc.edu>
2016-02-05 18:21:04 +0000

    Add tmux.conf and restore script

--- copy_files.sh
+++ copy_files.sh
@@ -7,5 +7,6 @@ cp ~/.gitconfig .gitconfig
 cp ~/.gitignore_global .gitignore_global
 cp ~/.vimrc .vimrc
 cp ~/.git-completion.bash .git-completion.bash
+cp ~/.tmux.conf .tmux.conf
 rm -rf .vim
 cp -rp ~/.vim .vim
---
+++ restore_files.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cp .bashrc ~/.bashrc
+cp .bash_profile ~/.bash_profile
+cp .profile ~/.profile
+cp .gitconfig ~/.gitconfig
+cp .gitignore_global ~/.gitignore_global
+cp .vimrc ~/.vimrc
+cp .git-completion.bash ~/.git-completion.bash
+cp .tmux.conf ~/.tmux.conf
+rm -rf ~/.vim
+cp -rp .vim ~/.vim

b9a4810e67fdd1ab61f55f1d86e79986c9f57785
Glen Horton <hortongn@ucmail.uc.edu>
2018-02-28 15:18:07 +0000

    Add iTerm config file

--- copy_files.sh
+++ copy_files.sh
@@ -7,5 +7,6 @@ cp ~/.gitignore_global .gitignore_global
 cp ~/.vimrc .vimrc
 cp ~/.git-completion.bash .git-completion.bash
 cp ~/.tmux.conf .tmux.conf
+cp ~/.iterm/com.googlecode.iterm2.plist com.googlecode.iterm2.plist
 rm -rf .vim
 cp -rp ~/.vim .vim

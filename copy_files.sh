f67fe945a124b1ac42af76cb030f97b24ba1380c
Glen Horton <hortongn@ucmail.uc.edu>
2018-07-30 13:47:17 +0000

    Don't complain when iterm config is missing

--- copy_files.sh
+++ copy_files.sh
@@ -7,7 +7,7 @@ cp ~/.gitignore_global .gitignore_global
 cp ~/.vimrc .vimrc
 cp ~/.git-completion.bash .git-completion.bash
 cp ~/.tmux.conf .tmux.conf
-cp ~/.iterm/com.googlecode.iterm2.plist com.googlecode.iterm2.plist
+cp ~/.iterm/com.googlecode.iterm2.plist com.googlecode.iterm2.plist 2>/dev/null
 rm -rf .vim
 cp -rp ~/.vim .vim
 cp ~/tdev.sh tdev.sh

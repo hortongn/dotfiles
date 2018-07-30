36fdee5c0e7333135d17bff09cd2f9821a2f3119
Glen Horton <hortongn@ucmail.uc.edu>
2018-07-30 13:49:55 +0000

    Update pane tmux layout

--- tdev.sh
+++ tdev.sh
@@ -6,14 +6,30 @@ then
   tmux new-session -As $directory_name
 else
   tmux -2 new-session -As $directory_name -d
+
   tmux split-window -v
   tmux select-pane -t 2
+  tmux resize-pane -t 2 -y 19
+
   tmux split-window -h
+  tmux resize-pane -t 2 -x 65
+
   tmux select-pane -t 2
-  tmux send-keys "bundle exec sidekiq" C-m
   tmux split-window -v
-  tmux send-keys "SKIP_RAILS=true bundle exec rails hydra:server" C-m
+  tmux resize-pane -t 2 -y 2
+
+  tmux select-pane -t 3
   tmux split-window -v
-  tmux select-pane -t 1
+  tmux resize-pane -t 3 -y 2
+
+  tmux select-pane -t 4
+  tmux split-window -v
+  tmux resize-pane -t 4 -y 3
+  
+  tmux send-keys -t 2 "solrdev" C-m
+  tmux send-keys -t 3 "fcdev" C-m
+  tmux send-keys -t 4 "bundle exec sidekiq" C-m
+  tmux send-keys -t 5 "rs" C-m
+  
   tmux -2 attach-session -t $directory_name
 fi
